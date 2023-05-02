fae26baa3e3c25dcfa6d53edb87a22e1d83a908d
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-05-02 21:52:50 +0200

    tests: add test for user interaction

--- libs/hilfreich/src/Widget.spec.tsx
+++ libs/hilfreich/src/Widget.spec.tsx
@@ -1,5 +1,6 @@
 import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/preact";
+import userEvent from "@testing-library/user-event";
 
 import Widget from "./Widget";
 
@@ -13,5 +14,27 @@ describe("Feedback Widget", () => {
 
     expect(screen.getByTestId("like-button")).toBeInTheDocument();
     expect(screen.getByTestId("dislike-button")).toBeInTheDocument();
+  });
+
+  it("allows to vote by clicking on the like or dislike button", async () => {
+    const props = { ...defaultProps, onVote: vi.fn() };
+
+    render(<Widget {...props} />);
+
+    expect(screen.getByTestId("like-button")).toBeInTheDocument();
+    await userEvent.click(screen.getByTestId("like-button"));
+
+    expect(props.onVote).toHaveBeenCalledWith({
+      isUseful: true,
+      sessionID: expect.any(String),
+    });
+
+    expect(screen.getByTestId("dislike-button")).toBeInTheDocument();
+    await userEvent.click(screen.getByTestId("dislike-button"));
+
+    expect(props.onVote).toHaveBeenCalledWith({
+      isUseful: false,
+      sessionID: expect.any(String),
+    });
   });
 });
